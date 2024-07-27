f6db5bb9feae8773d4a4a93d13b4df1aa568eba6
Dylan Beattie <dylan@dylanbeattie.net>
2024-07-28 00:00:44 +0100

    Apparently I can import from grammar.terms

--- codemirror-lang-rockstar/src/grammars/tokens.ts
+++ codemirror-lang-rockstar/src/grammars/tokens.ts
@@ -1,9 +1,24 @@
 import { ExternalTokenizer } from "@lezer/lr"
 import {
 	properVariable
-} from "./rockstar.grammar"
+} from "./rockstar.terms"
 
 export const matchProperVariable = new ExternalTokenizer((input, stack) => {
-	console.log(input);
-	console.log(input.next);
+	if (upperCodes.includes(input.next)) {
+		while(alphaCodes.includes(input.advance()));
+		acceptToken(properVariable);
+	}
 });
+
+
+const uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞĀĂĄĆĈĊČĎĐĒĔĖĘĚĜĞĠĢĤĦĨĪĬĮİĲĴĶĸĹĻĽĿŁŃŅŇŊŌŎŐŒŔŖŘŚŜŞŠŢŤŦŨŪŬŮŰŲŴŶŸŹŻŽ";
+const lowers = "abcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþāăąćĉċčďđēĕėęěĝğġģĥħĩīĭįi̇ĳĵķĸĺļľŀłńņňŋōŏőœŕŗřśŝşšţťŧũūŭůűųŵŷÿźżžŉß";
+const upperCodes = stringToCharCodeArray(uppers);
+const lowerCodes = stringToCharCodeArray(lowers);
+const alphaCodes = upperCodes.concat(lowerCodes);
+
+function stringToCharCodeArray(s) {
+	var result = [];
+	for(var i = 0; i < s.length; i++) result.push(s.charCodeAt(i));
+	return result;
+}

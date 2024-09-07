caf6ad8add9c8ae5180a1a40d2d904da8f5c32dc
Dylan Beattie <dylan@dylanbeattie.net>
2024-09-07 17:45:29 +0100

    FEAT: what happens if I tag a commit as FEAT? FIX didn't work :(

--- Starship/Rockstar.Engine/RockstarEnvironment.cs
+++ Starship/Rockstar.Engine/RockstarEnvironment.cs
@@ -10,6 +10,7 @@ public enum Scope {
 }
 
 public class RockstarEnvironment(IRockstarIO io) {
+
 	// This line will be automatically overwritten by GitHub Actions when the engine
 	// is built.
 	public const string VERSION = "_REPLACE_ME_";
